21d476a4263ca633d467ea867e488d456793be84
UULIN <1036190402@qq.com>
2020-07-20 14:36:24 +0800

    111

--- easy/test.py
+++ easy/test.py
@@ -1,7 +1,5 @@
-list1 = ["flower","flow","flight"]
-res = ""
-for temp in zip(*list1):
-    temp_set = set(temp)
-    if len(temp_set) == 1:
-        res += temp[0]
-print(res)
+s = 'sdfsd'
+
+if 'fs' in s:
+    s = s.strip('fs')
+print(s)
