ff7251697e4752ccfa20eb31a3f202c26a3c92b2
UULIN <1036190402@qq.com>
2020-07-27 16:21:17 +0800

    递归函数

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # my_leetcode
 # 个人刷题专用
 https://leetcode-cn.com/u/lin-xt/
+
+热度top100
+https://leetcode-cn.com/problemset/top/
---
+++ easy/21. 合并两个有序链表.py
@@ -0,0 +1,26 @@
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+示例：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1: return l2
+        if not l2: return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
