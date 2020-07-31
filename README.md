d943bbde75adee5fbdda4526b2c5c6cb421dee7a
UULIN <1036190402@qq.com>
2020-07-31 16:20:37 +0800

    111

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://leetcode-cn.com/u/lin-xt/
 
 热度top100
 https://leetcode-cn.com/problemset/top/
+
+# 待了解
+1、jstack、jmap命令使用及java堆栈内存分析
--- easy/test.py
+++ easy/test.py
@@ -1,3 +1,12 @@
-s = " "
-print(len(s))
-print(s.split())
+list1 = [1,2,3]
+a = [str(i) for i in list1]
+print(a)
+b = int(''.join(a))
+
+b += 1
+print(b)
+list2 = []
+for i in str(b):
+    print(i)
+    list2.append(i)
+print(list2)
